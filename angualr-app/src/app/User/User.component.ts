0296e5c8d04cca28d4f1db33e76022923d678e35
VisionYang1 <411525485@qq.com>
2019-07-23 22:27:55 +0100

    test

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -15,8 +15,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { UserService } from './User.service';
-import { Chart } from 'angular-highcharts';
+// import { Chart } from 'angular-highcharts';
 import 'rxjs/add/operator/toPromise';
+
+var Highcharts = require('highcharts');
+require('highcharts/modules/exporting')(Highcharts);
 
 @Component({
   selector: 'app-user',
@@ -57,28 +60,28 @@ export class UserComponent implements OnInit {
   };
 
 
-  chart = new Chart({
-    chart: {
-      type: 'line'
-    },
-    title: {
-      text: 'Linechart'
-    },
-    credits: {
-      enabled: false
-    },
-    series: [
-      {
-        name: 'Line 1',
-        data: [1, 2, 3]
-      }
-    ]
-  });
+  // chart = new Chart({
+  //   chart: {
+  //     type: 'line'
+  //   },
+  //   title: {
+  //     text: 'Linechart'
+  //   },
+  //   credits: {
+  //     enabled: false
+  //   },
+  //   series: [
+  //     {
+  //       name: 'Line 1',
+  //       data: [1, 2, 3]
+  //     }
+  //   ]
+  // });
 
-  // add point to chart serie
-  add() {
-    this.chart.addPoint(Math.floor(Math.random() * 10));
-  }
+  // // add point to chart serie
+  // add() {
+  //   this.chart.addPoint(Math.floor(Math.random() * 10));
+  // }
 
   ngOnInit(): void {
     this.loadAll();
