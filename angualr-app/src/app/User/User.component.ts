d03f6651afa74498d8f744a716cad0def6779314
VisionYang1 <411525485@qq.com>
2019-07-24 16:37:31 +0100

    aaa

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -86,10 +86,10 @@ export class UserComponent implements OnInit {
         type: 'column'
       },
       title: {
-        text: 'Monthly Average Rainfall'
+        text: 'Monthly Average Intake of Fruit and Vegetable'
       },
       subtitle: {
-        text: 'Source: WorldClimate.com'
+        text: ''
       },
       xAxis: {
         categories: [
@@ -111,13 +111,13 @@ export class UserComponent implements OnInit {
       yAxis: {
         min: 0,
         title: {
-          text: 'Rainfall (mm)'
+          text: 'Grams (g)'
         }
       },
       tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-          '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+          '<td style="padding:0"><b>{point.y:.1f} g</b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
@@ -129,11 +129,11 @@ export class UserComponent implements OnInit {
         }
       },
       series: [{
-        name: 'Tokyo',
+        name: 'Fruit',
         data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
     
       }, {
-        name: 'New York',
+        name: 'New Vegetable',
         data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
     
       }, {
