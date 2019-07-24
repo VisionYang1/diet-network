1589c793969d9623056cca773f5ac31fb76d023c
VisionYang1 <411525485@qq.com>
2019-07-24 17:26:56 +0100

    asgfasjhgig

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -46,12 +46,12 @@ export class UserComponent implements OnInit {
   cash = new FormControl('', Validators.required);
   apple = new FormControl('', Validators.required);
   reward = new FormControl('', Validators.required);
-  action = new FormControl('', Validators.required);
+  selectTime = new FormControl('', Validators.required);
 
 
   constructor(public serviceUser: UserService, fb: FormBuilder) {
     this.myForm = fb.group({
-      action: this.action,
+      selectTime: this.selectTime,
       userID: this.userID,
       firstName: this.firstName,
       lastName: this.lastName,
@@ -59,7 +59,7 @@ export class UserComponent implements OnInit {
       apple: this.apple,
       reward: this.reward
     });
-    this.myForm.get('action').setValue("perDay");
+    this.myForm.control['selectTime'].setValue("perDay");
   };
 
   // name = 'Angular 5';
