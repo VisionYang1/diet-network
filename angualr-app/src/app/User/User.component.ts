cb17d26c328195cb32c9f0121227e2d2906d1d73
VisionYang1 <411525485@qq.com>
2019-07-25 19:38:16 +0100

    asfashfjg

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -242,7 +242,7 @@ export class UserComponent implements OnInit {
       allTransactions.forEach(transaction => {
         tempList.push(transaction);
       });
-
+      this.getRangeDate(-6, "more");
       console.log("all transaction : " + tempList[0].appleInc);
     })
     .catch((error) => {
@@ -385,6 +385,47 @@ export class UserComponent implements OnInit {
         });
       });
     });
+  }
+
+  getRangeDate( range: number, type?: string ) {
+
+    const formatDate = ( time: any ) => {
+      // 格式化日期，获取今天的日期
+      const Dates = new Date( time );
+      const year: number = Dates.getFullYear();
+      const month: any = ( Dates.getMonth() + 1 ) < 10 ? '0' + ( Dates.getMonth() + 1 ) : ( Dates.getMonth() + 1 );
+      const day: any = Dates.getDate() < 10 ? '0' + Dates.getDate() : Dates.getDate();
+      return year + '-' + month + '-' + day;
+    };
+
+    const now = formatDate( new Date().getTime() ); // 当前时间
+    const resultArr: Array<any> = [];
+    let changeDate: string;
+    if ( range ) {
+      if ( type ) {
+        if ( type === 'one' ) {
+          changeDate = formatDate( new Date().getTime() + ( 1000 * 3600 * 24 * range ) );
+          console.log( changeDate );
+        }
+        if ( type === 'more' ) {
+          if ( range < 0 ) {
+            for ( let i = Math.abs( range ); i >= 0; i-- ) {
+              resultArr.push( formatDate( new Date().getTime() + ( -1000 * 3600 * 24 * i ) ) );
+              console.log( resultArr );
+            }
+          } else {
+            for ( let i = 1; i <= range; i++ ) {
+              resultArr.push( formatDate( new Date().getTime() + ( 1000 * 3600 * 24 * i ) ) );
+              console.log( resultArr );
+            }
+          }
+
+        }
+      } else {
+        changeDate = formatDate( new Date().getTime() + ( 1000 * 3600 * 24 * range ) );
+        console.log( changeDate );
+      }
+    }
   }
 
    updateParticipant(form: any): Promise<any> {
