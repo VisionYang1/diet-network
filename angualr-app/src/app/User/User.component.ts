6ee083ebbe4d019ff556f12abb5a192b9ca3a0ff
VisionYang1 <411525485@qq.com>
2019-07-27 16:59:02 +0100

    test day

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -60,6 +60,10 @@ export class UserComponent implements OnInit {
   private weekArray;
   private monthArray;
 
+  private dayData;
+  private weekData;
+  private monthData;
+
   private cashOJ;
   private fruitOJ;
   private vegetableOJ;
@@ -109,30 +113,30 @@ export class UserComponent implements OnInit {
         console.log("get user");
         // let user = this.currentId;
         console.log("myForm, user ID:" + this.myForm.get('userID').value);
-        this.getFruit(this.myForm);
+        this.getUserFruitTransaction(this.myForm, this.dayArray);
 
         //load graph according to fruit data
-        this.loadGraph(day, perDay);
+        this.loadGraph(day, perDay, this.dayData);
       }
   
       if (res=='perWeek')
       {
         let perWeek = ['First Week', 'Second Week', 'Thrid Week', 'Fourth Week'];
         let week = "Weekly";
-        this.loadGraph(week, perWeek);
+        this.loadGraph(week, perWeek, this.dayData);
       }
       if (res=='perMonth')
       {
         let perMonth = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
         let month = "Monthly";
-        this.loadGraph(month, perMonth);
+        this.loadGraph(month, perMonth, this.dayData);
       }
     });
 
     this.loadAll();
   }
 
-  loadGraph(title,time): void {
+  loadGraph(title, time, data): void {
     Highcharts.chart('container', {
       chart: {
         type: 'column'
@@ -169,19 +173,11 @@ export class UserComponent implements OnInit {
       },
       series: [{
         name: 'Fruit',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        data: data
     
       }, {
         name: 'Vegetable',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
-    
-      }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-    
-      }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+        data: data
     
       }]
     });
@@ -228,6 +224,12 @@ export class UserComponent implements OnInit {
           this.dayArray = this.getRangeDate(-6,this.dayArray, "more");
             break;
       }
+    }
+    else if(type == "week"){
+
+    }
+    else if(type == "month"){
+
     }
   }
 
@@ -372,37 +374,41 @@ export class UserComponent implements OnInit {
     });
   }
 
-  getFruit(form: any): Promise<any> {
+  getUserFruitTransaction(form: any, dateRange): Promise<any> {
 
 
     //retrieve all users in the tempList array
     let tempList = [];
-    let userAllTransaction = [];
+    let userAllFruitTransaction = [];
 
     // call transaction
     return this.serviceUser.getAllFruitTransactions()
     .toPromise()
-    .then((allTransactions) => {
+    .then((allFruitTransactions) => {
       
       this.errorMessage = null;
 
-      allTransactions.forEach(transaction => {
+      allFruitTransactions.forEach(transaction => {
         tempList.push(transaction);
       });
 
-      // get the given user's transaction
+      // get the given user's fruit transaction
+      let userFruitID = "resource:org.diet.network.Fruit#" + form.get('userID').value;
       for(let transaction of tempList){
-        if(transaction.appleInc == "resource:org.diet.network.Apples#AP_1"){
-          userAllTransaction.push(transaction);
+        if(transaction.fruitInc == userFruitID){
+          userAllFruitTransaction.push(transaction);
         }
       }
 
-
+      //get given day transaction
+      for(let day of dateRange){
+        console.log("test day array:" + day);
+      }
 
       // this.getRangeDate(-6, "more");
-      console.log("all transaction : " + tempList[0].appleInc);
+      // console.log("all transaction : " + tempList[0].appleInc);
       // let time = tempList[0].timestamp.split("T", 2);
-      console.log("all transaction : " + userAllTransaction[0].appleInc);
+      // console.log("all transaction : " + allFruitTransactions[0].appleInc);
     })
     .catch((error) => {
       if(error == 'Server error'){
