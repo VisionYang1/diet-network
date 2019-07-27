23c401896ca5e3d62a8b7f2d690bce0b131eeb3b
VisionYang1 <411525485@qq.com>
2019-07-27 17:19:08 +0100

    ajkjakgjhagsda

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -393,7 +393,7 @@ export class UserComponent implements OnInit {
       });
 
       // get the given user's fruit transaction
-      let userFruitID = "resource:org.diet.network.Fruit#" + form.get('userID').value;
+      let userFruitID = "resource:org.diet.network.Fruit#FR_" + form.get('userID').value;
       console.log("user fruit id:" + userFruitID);
       for(let transaction of tempList){
         if(transaction.fruitInc == userFruitID){
