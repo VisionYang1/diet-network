4e514886effe6f5d9d72be31b98b1f7f1aa01c6b
VisionYang1 <411525485@qq.com>
2019-07-27 18:19:42 +0100

    asjkfhkqghgkaas

--- angualr-app/src/app/User/User.component.ts
+++ angualr-app/src/app/User/User.component.ts
@@ -425,6 +425,7 @@ export class UserComponent implements OnInit {
         }
         // console.log("test day array:" + day);
       }
+      this.dayData = dayTotalData;
       console.log("test day array data:" + dayTotalData);
 
       // this.getRangeDate(-6, "more");
